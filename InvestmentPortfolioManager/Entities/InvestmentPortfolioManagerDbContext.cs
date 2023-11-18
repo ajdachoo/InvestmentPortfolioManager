5024947a02d0984916d575d15f1de8dc7ca0cb03
Adam Cieslak <adamcieslak00@gmail.com>
2023-11-18 22:50:51 +0100

    add db context

---
+++ InvestmentPortfolioManager/Entities/InvestmentPortfolioManagerDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InvestmentPortfolioManager.Entities
+{
+    public class InvestmentPortfolioManagerDbContext : DbContext
+    {
+        public DbSet<Asset> Assets { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+
+        public InvestmentPortfolioManagerDbContext(DbContextOptions<InvestmentPortfolioManagerDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Asset>(eb =>
+            {
+                eb.Property(a => a.Ticker).IsRequired();
+                eb.Property(a => a.Price).IsRequired();
+                eb.Property(a => a.Currency).IsRequired();
+                eb.Property(a => a.Category).IsRequired();
+                eb.Property(a => a.Name).IsRequired();
+                eb.Property(a => a.UpdatedDate).IsRequired();
+            });
+
+            modelBuilder.Entity<Position>(eb =>
+            {
+                eb.Property(p => p.Quantity).IsRequired();
+                eb.Property(p => p.Status).IsRequired();
+                eb.Property(p => p.InitialValue).IsRequired();
+                eb.Property(p => p.CreatedDate).IsRequired();
+                eb.Property(p => p.UpdatedDate).IsRequired();
+            });
+
+            modelBuilder.Entity<User>(eb =>
+            {
+                eb.Property(u => u.Status).IsRequired();
+                eb.Property(u => u.Currency).IsRequired();
+                eb.Property(u => u.FirstName).IsRequired();
+                eb.Property(u => u.Email).IsRequired();
+                eb.Property(u => u.LastName).IsRequired();
+                eb.Property(u => u.PasswordHash).IsRequired();
+            });
+
+            modelBuilder.Entity<Wallet>(eb =>
+            {
+                eb.Property(w => w.CreatedDate).IsRequired();
+                eb.Property(w => w.Details).IsRequired();
+                eb.Property(w => w.UpdatedDate).IsRequired();
+                eb.Property(w => w.Name).IsRequired();
+            });
+        }
+    }
+}
